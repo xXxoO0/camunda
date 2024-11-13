10ea629478e527f3bd1d9b9ebe102e0542542c0d
Lena Schönburg <lena@fairburg.xyz>
2024-11-13 16:58:43 +0100

    feat: store newly created deployments in state

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/EventAppliers.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/EventAppliers.java
@@ -149,7 +149,8 @@ public final class EventAppliers implements EventApplier {
 
     register(
         DeploymentIntent.CREATED, 1, new DeploymentCreatedV1Applier(state.getDeploymentState()));
-    register(DeploymentIntent.CREATED, 2, new DeploymentCreatedV2Applier());
+    register(
+        DeploymentIntent.CREATED, 2, new DeploymentCreatedV2Applier(state.getDeploymentState()));
     register(
         DeploymentIntent.DISTRIBUTED,
         new DeploymentDistributedApplier(state.getProcessState(), state.getDecisionState()));
